4583da694156832c2459036e4d45fe1f89603303
Levin <github@17d.me>
2024-12-09 22:24:41 +0100

    Add hydrator stations_nearby

--- tests/JsonTest.php
+++ tests/JsonTest.php
@@ -3,6 +3,7 @@
 declare(strict_types=1);
 
 use DRM\Dto\Location;
+use DRM\Dto\Stop;
 use DRM\Dto\Trip;
 use DRM\Hydrator\DbRestHydrator;
 use PHPUnit\Framework\TestCase;
@@ -32,9 +33,23 @@ class JsonTest extends TestCase
 
         $locations = [];
         foreach ($data as $location) {
-            $locations[] = (new DbRestHydrator())->hydrate($location, \DRM\Dto\Stop::class);
+            $locations[] = (new DbRestHydrator())->hydrate($location, Stop::class);
         }
         $locations = json_encode($locations, JSON_PRETTY_PRINT);
         $this->assertJsonStringEqualsJsonString($json, $locations);
+    }
+
+    public function testJsonStationsNearbyIdentical()
+    {
+        $json = file_get_contents(__DIR__ . '/_data/stations_nearby.json');
+        $data = json_decode($json, true);
+        $this->assertIsArray($data);
+
+        $stations = [];
+        foreach ($data as $station) {
+            $stations[] = (new DbRestHydrator())->hydrate($station, Stop::class);
+        }
+        $stations = json_encode($stations, JSON_PRETTY_PRINT);
+        $this->assertJsonStringEqualsJsonString($json, $stations);
     }
 }
