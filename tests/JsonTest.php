31929259248f354d3c7fce58cb558b30ae5d3dbb
Levin <github@17d.me>
2024-12-09 23:05:50 +0100

    improve tests

--- tests/JsonTest.php
+++ tests/JsonTest.php
@@ -6,107 +6,56 @@ use DRM\Dto\FullStation;
 use DRM\Dto\Stop;
 use DRM\Dto\Trip;
 use DRM\Hydrator\DbRestHydrator;
+use PHPUnit\Framework\Attributes\DataProvider;
 use PHPUnit\Framework\TestCase;
 
 class JsonTest extends TestCase
 {
-    public static function jsonFiles()
+
+    public static function jsonDataProvider(): array
     {
         return [
-            ['departures.json'],
-            ['departures_2.json'],
+            ['departures.json', Trip::class],
+            ['departures_2.json', Trip::class],
+            ['locations.json', Stop::class],
+            ['stations_nearby.json', Stop::class],
         ];
     }
 
-    public static function stationsFiles()
+    public static function singleObjectJsonDataProvider(): array
     {
         return [
-            ['stations_ril.json'],
-            ['stations_ibnr.json']
+            ['stations_ril.json', FullStation::class],
+            ['stations_ibnr.json', FullStation::class],
+            ['stops_station.json', Stop::class],
+            ['stops_stop.json', Stop::class],
         ];
     }
 
-    /**
-     * // test with departures.json and departures_2.json
-     * @dataProvider jsonFiles
-     */
-    public function testJsonIdentical($file)
+    #[DataProvider('jsonDataProvider')]
+    public function testJson($file, $class)
     {
         $json = file_get_contents(__DIR__ . '/_data/'. $file);
         $data = json_decode($json, true);
         $this->assertIsArray($data);
 
-        //$trips = array_map(fn($trip) => new Trip($trip), $data);
         $trips = [];
         foreach ($data as $trip) {
-            $trips[] = (new DbRestHydrator())->hydrate($trip, Trip::class);
+            $trips[] = (new DbRestHydrator())->hydrate($trip, $class);
         }
         $trips = json_encode($trips, JSON_PRETTY_PRINT);
         $this->assertJsonStringEqualsJsonString($json, $trips);
     }
 
-    public function testJsonLocationsIdentical()
+    #[DataProvider('singleObjectJsonDataProvider')]
+    public function testObjectJsons($file, $class)
     {
-        $json = file_get_contents(__DIR__ . '/_data/locations.json');
+        $json = file_get_contents(__DIR__ . '/_data/'. $file);
         $data = json_decode($json, true);
         $this->assertIsArray($data);
 
-        $locations = [];
-        foreach ($data as $location) {
-            $locations[] = (new DbRestHydrator())->hydrate($location, Stop::class);
-        }
-        $locations = json_encode($locations, JSON_PRETTY_PRINT);
-        $this->assertJsonStringEqualsJsonString($json, $locations);
-    }
-
-    public function testJsonStationsNearbyIdentical()
-    {
-        $json = file_get_contents(__DIR__ . '/_data/stations_nearby.json');
-        $data = json_decode($json, true);
-        $this->assertIsArray($data);
-
-        $stations = [];
-        foreach ($data as $station) {
-            $stations[] = (new DbRestHydrator())->hydrate($station, Stop::class);
-        }
-        $stations = json_encode($stations, JSON_PRETTY_PRINT);
-        $this->assertJsonStringEqualsJsonString($json, $stations);
-    }
-
-
-    /**
-     * @dataProvider stationsFiles
-     */
-    public function testJsonStationsIdentical($file)
-    {
-        $json = file_get_contents(__DIR__ . '/_data/stations_ril.json');
-        $data = json_decode($json, true);
-        $this->assertIsArray($data);
-
-        $station = (new DbRestHydrator())->hydrate($data, FullStation::class);
-        $station = json_encode($station, JSON_PRETTY_PRINT);
-        $this->assertJsonStringEqualsJsonString($json, $station);
-    }
-
-    public function testJsonStopsStationIdentical()
-    {
-        $json = file_get_contents(__DIR__ . '/_data/stops_station.json');
-        $data = json_decode($json, true);
-        $this->assertIsArray($data);
-
-        $stops = (new DbRestHydrator())->hydrate($data, Stop::class);
-        $stops = json_encode($stops, JSON_PRETTY_PRINT);
-        $this->assertJsonStringEqualsJsonString($json, $stops);
-    }
-
-    public function testJsonStopsStopIdentical()
-    {
-        $json = file_get_contents(__DIR__ . '/_data/stops_stop.json');
-        $data = json_decode($json, true);
-        $this->assertIsArray($data);
-
-        $stop = (new DbRestHydrator())->hydrate($data, Stop::class);
-        $stop = json_encode($stop, JSON_PRETTY_PRINT);
-        $this->assertJsonStringEqualsJsonString($json, $stop);
+        $object = (new DbRestHydrator())->hydrate($data, $class);
+        $object = json_encode($object, JSON_PRETTY_PRINT);
+        $this->assertJsonStringEqualsJsonString($json, $object);
     }
 }
